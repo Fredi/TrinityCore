c3edb9a78831d4135d9dd799fddbc98d0daff5b2
Fredi Machado <fredisoft@gmail.com>
2011-06-07 16:15:54 -0300

    Argent Tournament: Forgot to add a couple of scripts

--- sql/custom/argent_tournament.sql
+++ sql/custom/argent_tournament.sql
@@ -17,6 +17,12 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_squire_danny' WHERE `entry` =
 -- npc_argent_champion
 UPDATE `creature_template` SET `ScriptName` = 'npc_argent_champion' WHERE `entry` = 33707;
 
+-- npc_ring_champions
+UPDATE `creature_template` SET `ScriptName` = 'npc_ring_champions' WHERE `entry` IN (33738,33746,33747,33743,33740,33748,33744,33745,33749,33739);
+-- npc_ring_valiants
+UPDATE `creature_template` SET `flags_extra` = 0, `AIName` = '', `ScriptName` = 'npc_ring_valiants' WHERE `entry` IN (33384,33306,33285,33382,33383,33558,33564,33561,33562,33559);
+
+
 -- npc_lake_frog
 UPDATE `creature_template` SET `ScriptName` = 'npc_lake_frog' WHERE `entry` IN (33211,33224);
 UPDATE `creature_template` SET `gossip_menu_id` = 33220 WHERE `entry` = 33220;
