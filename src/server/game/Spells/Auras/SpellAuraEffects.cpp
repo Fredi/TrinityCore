2e97c68116677240cf3031ff357d78471afb5e5d
Fredi Machado <fredisoft@gmail.com>
2011-06-07 17:03:24 -0300

    Added a AutoTransform taken from Mangos

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -3445,6 +3445,9 @@ void AuraEffect::HandleAuraTransform(AuraApplication const * aurApp, uint8 mode,
                     case 65495:
                         target->SetDisplayId(target->getGender() == GENDER_MALE ? 29203 : 29204);
                         break;
+                    case 71450:                                 // Crown Parcel Service Uniform
+                        target->SetDisplayId(target->getGender() == GENDER_MALE ? 31002 : 31003);
+                        break;
                     // Darkspear Pride
                     case 75532:
                         target->SetDisplayId(target->getGender() == GENDER_MALE ? 31737 : 31738);
