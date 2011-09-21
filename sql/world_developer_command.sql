1b23d77259a96d95ab452a15ecc19e349141949b
Fredi Machado <fredisoft@gmail.com>
2011-09-21 18:09:19 -0300

    Revert "Command to set <Dev> tag instead of <GM>. By Rochet2"
    This reverts commit 160c5b3cb32767c265ef5ca7af59d891f0c8a83f.

--- sql/world_developer_command.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `command` WHERE `name` = 'developer';
-INSERT INTO `command` VALUES
-('developer',2,'Syntax: .developer [on/off]\r\n\r\nEnable or Disable in game Dev tag or show current state if on/off not provided.');
