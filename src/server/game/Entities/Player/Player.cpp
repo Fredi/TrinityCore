a14e466b049cf88fc36b9af4ea66196d93b6c029
Aios <aioslike@gmail.com>
2011-11-21 06:04:26 +0100

    Core/Spells: Hackfix hunter ranged autoattack invisible exploit

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1657,6 +1657,10 @@ void Player::Update(uint32 p_time)
         }
     }
 
+    //Fix invisible exploit hunters when ranged attack
+    if (getAttackTimer(RANGED_ATTACK) && HasAura(58984))
+        RemoveAura(58984);
+
     if (HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_RESTING))
     {
         if (roll_chance_i(3) && GetTimeInnEnter() > 0)      // freeze update
